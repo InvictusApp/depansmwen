06b583d80a5405cb6ca24fc88d04e21b48895ed9
steelanterns <steevejeanchilles@gmail.com>
2021-01-06 05:42:49 -0500

    stee-second commit

---
+++ app/src/main/java/com/klasscode/depansmwen/controller/cash/MainActivityCash.java
@@ -0,0 +1,15 @@
+package com.klasscode.depansmwen.controller.cash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.klasscode.depansmwen.R;
+
+public class MainActivityCash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_cash);
+    }
+}
